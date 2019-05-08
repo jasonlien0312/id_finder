6ad971c9e60358ff0277484b33c56f47ac0a86ff
Jason Lien <lienshao@tamu.edu>
2019-05-08 17:46:21 -0500

    avoid text input

--- id_finder.py
+++ id_finder.py
@@ -1,6 +1,23 @@
 import csv
 import pprint as pp
-def get_id(q):
+def load_data(csv_data):
+    # load data
+    csv_data = "sample_csv.csv" # change this
+    name_to_id = {}
+    addr_to_id = {}
+    count = 1
+    with open(csv_data, mode='r') as f:
+        csv_file = csv.reader(f, delimiter=',', quotechar='"')
+        for r in csv_file:
+            if count == 1:
+                count += 1 # skip the first line
+            else:
+                name_to_id[r[5]] = r[2] # add name and id into dict
+                # addr = " ".join((r[24].split())) # process the address
+                addr_to_id[r[24]] = r[2] # add address and id into dict
+    return name_to_id,addr_to_id
+
+def get_id(name_to_id,addr_to_id,q):
     # gets the id
     if q[0].isdigit():
         # address case
@@ -12,21 +29,7 @@ def get_id(q):
         # name case
         return name_to_id[q]
 
-# load data
-csv_data = "sample_csv.csv" # change this
-name_to_id = {}
-addr_to_id = {}
-count = 1
-with open(csv_data, mode='r') as f:
-    csv_file = csv.reader(f, delimiter=',', quotechar='"')
-    for r in csv_file:
-        if count == 1:
-            count += 1 # skip the first line
-        else:
-            name_to_id[r[5]] = r[2] # add name and id into dict
-            # addr = " ".join((r[24].split())) # process the address
-            addr_to_id[r[24]] = r[2] # add address and id into dict
-
 # ask for input
-query = input("Please enter name or address: ")
-print("Your answer to that is:",get_id(query))
+name, addr = load_data("sample_csv.csv")
+get_id(name,addr,"query")
+    
